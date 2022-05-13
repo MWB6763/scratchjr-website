b485d0a98017c58ebfe0bbbae904711427792e0b
MWB6763 <77411288+MWB6763@users.noreply.github.com>
2022-05-13 17:57:58 -0400

    Update triggers.jsx

--- src/views/learn/tips/triggers.jsx
+++ src/views/learn/tips/triggers.jsx
@@ -11,7 +11,7 @@ const TriggersSection = () => (
             <iframe
                 width="640"
                 height="480"
-                src="https://www.youtube.com/embed/123AdwR_JxI?rel=0"
+                src="https://youtube.com/embed/123AdwR_JxI?rel=0"
                 frameBorder="0"
                 allowFullScreen
             />
