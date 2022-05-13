ad9a3b93db1026cd5936bdf21ea0114b8a39ac6f
MWB6763 <77411288+MWB6763@users.noreply.github.com>
2022-05-13 18:14:06 -0400

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -14,31 +14,31 @@ const Footer = () => (
                     href="https://twitter.com/ScratchJr"
                     rel="noopener noreferrer"
                     target="_blank"
-                    title="Follow us on Twitter"
+                    title="Twitter"
                 >
                     <span>Twitter</span>
                 </a>
                 <a
                     className="social-link sjr-icon-facebook"
-                    href="https://www.facebook.com/scratchjr"
+                    href="https://facebook.com/scratchjr"
                     rel="noopener noreferrer"
                     target="_blank"
-                    title="Like us on Facebook"
+                    title="Facebook"
                 >
                     <span>Facebook</span>
                 </a>
             </div>
             <div id="footer-nav">
-                <a href="http://eepurl.com/3ytf5">
+                <a href="https://eepurl.com/3ytf5" title="Join Mailing List">
                     <div className="blue-button">Join Mailing List</div>
                 </a>
                 &nbsp;
-                <a href="mailto:info@scratchjr.org">
+                <a href="mailto:info@scratchjr.org" title="Contact Us">
                     <div className="blue-button">Contact Us</div>
                 </a>
             </div>
         </div>
-        <div className="footer-links">
+        <div className="footer-links" title="Privacy Policy">
             <a href="/privacy">Privacy Policy</a>
         </div>
     </div>
