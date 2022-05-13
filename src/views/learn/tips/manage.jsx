f1d501abcd51de9ec39061b2a0caaaa35815456c
MWB6763 <77411288+MWB6763@users.noreply.github.com>
2022-05-13 17:55:46 -0400

    Update manage.jsx

--- src/views/learn/tips/manage.jsx
+++ src/views/learn/tips/manage.jsx
@@ -4,7 +4,7 @@ import Section from '../../../components/sectionitem/section.jsx';
 const ManageSection = () => (
     <Section
         id="manage-projects-section"
-        title="Making, Renaming, and Deleting Projects"
+        title="Making, Renaming, And Deleting Projects"
     >
         <div className="content-section-description">
             Watch the video to learn how to make a new project, rename
@@ -14,7 +14,7 @@ const ManageSection = () => (
             <iframe
                 width="640"
                 height="480"
-                src="https://www.youtube.com/embed/RXXDUxqBzBI?rel=0"
+                src="https://youtube.com/embed/RXXDUxqBzBI?rel=0"
                 frameBorder="0"
                 allowFullScreen
             />
