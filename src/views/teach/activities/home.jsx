960fed74a5729b3b96af208fab07610d97413365
MWB6763 <77411288+MWB6763@users.noreply.github.com>
2022-05-13 17:46:32 -0400

    Update home.jsx

--- src/views/teach/activities/home.jsx
+++ src/views/teach/activities/home.jsx
@@ -16,7 +16,7 @@ const ActivitiesHome = () => (
             to do new things with ScratchJr. They are listed here in
             order of simplest to hardest, but feel free to play around
             in any order you&apos;d like! For more information and a brief
-            introduction to ScratchJr see the <a href="/teach/resources">Resources page</a>.
+            introduction to ScratchJr see the <a href="/teach/resources" title="Resources page">resources page</a>.
         </div>
         <div className="content-section-items-container">
             <LinkedSectionItem
@@ -24,7 +24,7 @@ const ActivitiesHome = () => (
                 format="half"
                 thumbnail="/images/activitycards/card01-car.png"
                 description="Pick a background and a character, and use a motion block to make a car drive across the city..." // eslint-disable-line max-len
-                linkText="Read more"
+                linkText="Read More"
                 linkURL="/activities/drive-across-the-city"
             />
             <LinkedSectionItem
@@ -32,7 +32,7 @@ const ActivitiesHome = () => (
                 format="half"
                 thumbnail="/images/activitycards/card03-race.png"
                 description="Use the speed block to speed up or slow down a character..."
-                linkText="Read more"
+                linkText="Read More"
                 linkURL="/activities/run-a-race"
             />
             <LinkedSectionItem
@@ -40,7 +40,7 @@ const ActivitiesHome = () => (
                 format="half"
                 thumbnail="/images/activitycards/card06-sun.png"
                 description="Learn how to make a character disappear..."
-                linkText="Read more"
+                linkText="Read More"
                 linkURL="/activities/sunset"
             />
             <LinkedSectionItem
@@ -48,7 +48,7 @@ const ActivitiesHome = () => (
                 format="half"
                 thumbnail="/images/activitycards/card07-moon.png"
                 description="Learn how to add a new page to change scene..."
-                linkText="Read more"
+                linkText="Read More"
                 linkURL="/activities/moonrise-after-sunset"
             />
             <LinkedSectionItem
@@ -56,7 +56,7 @@ const ActivitiesHome = () => (
                 format="half"
                 thumbnail="/images/activitycards/card05-forest.png"
                 description="Make multiple characters with their own scripts..."
-                linkText="Read more"
+                linkText="Read More"
                 linkURL="/activities/spooky-forest"
             />
             <LinkedSectionItem
@@ -64,7 +64,7 @@ const ActivitiesHome = () => (
                 format="half"
                 thumbnail="/images/activitycards/card04-dribble.png"
                 description="Learn how to use the repeat block to dribble a basketball..."
-                linkText="Read more"
+                linkText="Read More"
                 linkURL="/activities/dribble-a-basketball"
             />
             <LinkedSectionItem
@@ -72,7 +72,7 @@ const ActivitiesHome = () => (
                 format="half"
                 thumbnail="/images/activitycards/card02-dance.png"
                 description="Use sound and motion blocks and a start again block to make characters dance..."
-                linkText="Read more"
+                linkText="Read More"
                 linkURL="/activities/dance-party"
             />
             <LinkedSectionItem
@@ -80,7 +80,7 @@ const ActivitiesHome = () => (
                 format="half"
                 thumbnail="/images/activitycards/card08-greet.png"
                 description="Use the envelope to send a message from one character to another..."
-                linkText="Read more"
+                linkText="Read More"
                 linkURL="/activities/meet-and-greet"
             />
             <LinkedSectionItem
@@ -88,7 +88,7 @@ const ActivitiesHome = () => (
                 format="half"
                 thumbnail="/images/activitycards/card09-talk.png"
                 description="Send multiple messages among characters using different colored envelopes..."
-                linkText="Read more"
+                linkText="Read More"
                 linkURL="/activities/conversation"
             />
         </div>
