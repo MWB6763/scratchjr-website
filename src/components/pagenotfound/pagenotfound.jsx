29bb89116eec045d43978d886016fd69860a8437
MWB6763 <77411288+MWB6763@users.noreply.github.com>
2022-05-13 18:15:13 -0400

    Update pagenotfound.jsx

--- src/components/pagenotfound/pagenotfound.jsx
+++ src/components/pagenotfound/pagenotfound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PageNotFound = () => (
     <div className="content-section">
         <h1>Page Not Found</h1>
-        <p>Go to the <a href="/">Home</a> page.</p>
+        <p>Go to the <a href="/" title="Home">Home</a> page.</p>
     </div>
 );
 export default PageNotFound;
