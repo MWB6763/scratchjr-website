fa47e9d11807663ec1b8ca9c5c0fa035fad99f34
MWB6763 <77411288+MWB6763@users.noreply.github.com>
2022-05-13 17:54:33 -0400

    Update animation.jsx

--- src/views/learn/tips/animation.jsx
+++ src/views/learn/tips/animation.jsx
@@ -4,14 +4,14 @@ import Section from '../../../components/sectionitem/section.jsx';
 const AnimationSection = () => (
     <Section
         id="character-animation-section"
-        title="Character Animation using the ScratchJr Blocks"
+        title="Character Animation Using The ScratchJr Blocks"
         description="See how to make a simple script by connecting blocks together."
     >
         <div className="content-section-video">
             <iframe
                 width="640"
                 height="480"
-                src="https://www.youtube.com/embed/JoHpVzltafU?rel=0"
+                src="https://youtube.com/embed/JoHpVzltafU?rel=0"
                 frameBorder="0"
                 allowFullScreen
             />
